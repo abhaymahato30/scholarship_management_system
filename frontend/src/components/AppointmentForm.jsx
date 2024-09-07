6d87ee5df9d37c48dc635d46c4e8c70ae9f1ee92
Abhay Kumar Mahato <124015835+abhaymahato30@users.noreply.github.com>
2024-09-07 12:02:43 +0530

    Update AppointmentForm.jsx

--- frontend/src/components/AppointmentForm.jsx
+++ frontend/src/components/AppointmentForm.jsx
@@ -44,7 +44,7 @@ const AppointmentForm = () => {
       
         formData.append("incomedocAvatar", incomedocAvatar);
         await axios
-          .post("http://localhost:4000/api/v1/studentinfo/post", formData, {
+          .post("https://scholarship-management-system-dy3i.vercel.app/api/v1/studentinfo/post", formData, {
             withCredentials: true,
             headers: { "Content-Type": "multipart/form-data" },
           })
